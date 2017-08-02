5cde430a48a6a4fd851a1972e1f8a3cc30d14073
Luis Pabón <luis.pabon@coreos.com>
2017-08-02 11:08:42 -0400

    patch

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
+setup_rkt() {
+    if ! command -v rkt > /dev/null ; then
+        dnf -y install https://github.com/rkt/rkt/releases/download/v1.28.0/rkt-1.28.0-1.x86_64.rpm
+
+        mkdir -p /etc/rkt/net.d
+		cat > /etc/rkt/net.d/20-metal.conf << EOF
+{
+  "name": "metal0",
+  "type": "bridge",
+  "bridge": "metal0",
+  "isGateway": true,
+  "ipMasq": true,
+  "ipam": {
+    "type": "host-local",
+    "subnet": "172.18.0.0/24",
+    "routes" : [ { "dst" : "0.0.0.0/0" } ]
+   }
+}
+EOF
+
+    fi
+}
+
 get_matchbox() {
     git clone https://github.com/coreos/matchbox.git
 }
