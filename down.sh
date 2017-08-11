a3b109ef9a9323fc194cc50a4253861229e0cfd9
Luis Pabón <luis.pabon@coreos.com>
2017-08-11 01:38:02 -0400

    update down

--- down.sh
+++ down.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd matchbox
 echo "--> Shutdown and destroy nodes"
-sudo ./scripts/libvirt destroy
+sudo ../scripts/libvirt destroy
 echo "--> Shutdown and destroy pxe"
 sudo CONTAINER_RUNTIME=docker ./scripts/devnet destroy
