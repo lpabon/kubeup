888f98f26644210e09de93138bb4a3208a42853c
Luis Pabón <luis@portworx.com>
2021-01-17 11:07:48 -0800

    global_vars template

--- up.sh
+++ up.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+if [ ! global_vars.yml ] ; then
+	cp global_vars.yml.tmpl global_vars.yml
+fi
+
 vagrant up --provider=libvirt --no-provision $@ \
     && vagrant --provider=libvirt provision
